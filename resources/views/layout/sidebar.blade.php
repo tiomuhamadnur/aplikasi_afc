8e49eb8939ce814693cda3727fd76b73885e2bf2
Tio Muhamad Nur <mtio@jakartamrt.co.id>
2024-06-27 01:34:57 +0700

    update: log afc

--- resources/views/layout/sidebar.blade.php
+++ resources/views/layout/sidebar.blade.php
@@ -44,64 +44,71 @@
                 <i class="mdi mdi-receipt menu-icon"></i>
             </a>
         </li>
-        <li class="nav-item">
-            <a class="nav-link" data-bs-toggle="collapse" href="#ui-basic" aria-expanded="false"
-                aria-controls="ui-basic">
-                <span class="menu-title">Master Data</span>
-                <i class="menu-arrow"></i>
-                <i class="mdi mdi-database menu-icon"></i>
-            </a>
-            <div class="collapse" id="ui-basic">
-                <ul class="nav flex-column sub-menu">
-                    <li class="nav-item"> <a class="nav-link" href="{{ route('user.index') }}">User</a></li>
-                    <li class="nav-item"> <a class="nav-link" href="{{ route('lokasi.index') }}">Lokasi</a></li>
-                    <li class="nav-item"> <a class="nav-link" href="{{ route('sub-lokasi.index') }}">Sub Lokasi</a></li>
-                    <li class="nav-item"> <a class="nav-link" href="{{ route('detail-lokasi.index') }}">Detail
-                            Lokasi</a></li>
-                    <li class="nav-item"> <a class="nav-link" href="{{ route('area.index') }}">Relasi Area</a>
-                    </li>
-                    <li class="nav-item"> <a class="nav-link" href="{{ route('direktorat.index') }}">Direktorat</a>
-                    </li>
-                    <li class="nav-item"> <a class="nav-link" href="{{ route('divisi.index') }}">Divisi</a>
-                    </li>
-                    <li class="nav-item"> <a class="nav-link" href="{{ route('departemen.index') }}">Departemen</a>
-                    </li>
-                    <li class="nav-item"> <a class="nav-link" href="{{ route('seksi.index') }}">Seksi</a>
-                    </li>
-                    <li class="nav-item"> <a class="nav-link" href="{{ route('struktur.index') }}">Relasi Struktur</a>
-                    </li>
-                    <li class="nav-item"> <a class="nav-link" href="{{ route('tipe-barang.index') }}">Tipe Barang</a>
-                    </li>
-                    <li class="nav-item"> <a class="nav-link" href="{{ route('tipe-pekerjaan.index') }}">Tipe
-                            Pekerjaan</a>
-                    </li>
-                    <li class="nav-item"> <a class="nav-link" href="{{ route('tipe-permit.index') }}">Tipe
-                            Permit</a>
-                    </li>
-                    <li class="nav-item"> <a class="nav-link" href="{{ route('tipe-equipment.index') }}">Tipe
-                            Equipment</a>
-                    </li>
-                    <li class="nav-item"> <a class="nav-link" href="{{ route('tipe-employee.index') }}">Tipe
-                            Employee</a>
-                    </li>
-                    <li class="nav-item"> <a class="nav-link" href="{{ route('satuan.index') }}">Satuan</a>
-                    </li>
-                    <li class="nav-item"> <a class="nav-link" href="{{ route('gender.index') }}">Gender</a>
-                    </li>
-                    <li class="nav-item"> <a class="nav-link" href="{{ route('jabatan.index') }}">Jabatan</a>
-                    </li>
-                    <li class="nav-item"> <a class="nav-link" href="{{ route('role.index') }}">Role</a>
-                    </li>
-                    <li class="nav-item"> <a class="nav-link" href="{{ route('arah.index') }}">Arah</a>
-                    </li>
-                    <li class="nav-item"> <a class="nav-link" href="{{ route('perusahaan.index') }}">Perusahaan</a>
-                    </li>
-                    <li class="nav-item"> <a class="nav-link" href="{{ route('barang.index') }}">Barang</a>
-                    </li>
-                    <li class="nav-item"> <a class="nav-link" href="{{ route('equipment.index') }}">Equipment</a>
-                    </li>
-                </ul>
-            </div>
-        </li>
+        @if (auth()->user()->role->id == 1)
+            <li class="nav-item">
+                <a class="nav-link" data-bs-toggle="collapse" href="#ui-basic" aria-expanded="false"
+                    aria-controls="ui-basic">
+                    <span class="menu-title">Master Data</span>
+                    <i class="menu-arrow"></i>
+                    <i class="mdi mdi-database menu-icon"></i>
+                </a>
+                <div class="collapse" id="ui-basic">
+                    <ul class="nav flex-column sub-menu">
+                        <li class="nav-item"> <a class="nav-link" href="{{ route('user.index') }}">User</a></li>
+                        <li class="nav-item"> <a class="nav-link" href="{{ route('lokasi.index') }}">Lokasi</a></li>
+                        <li class="nav-item"> <a class="nav-link" href="{{ route('sub-lokasi.index') }}">Sub Lokasi</a>
+                        </li>
+                        <li class="nav-item"> <a class="nav-link" href="{{ route('detail-lokasi.index') }}">Detail
+                                Lokasi</a></li>
+                        <li class="nav-item"> <a class="nav-link" href="{{ route('area.index') }}">Relasi Area</a>
+                        </li>
+                        <li class="nav-item"> <a class="nav-link" href="{{ route('direktorat.index') }}">Direktorat</a>
+                        </li>
+                        <li class="nav-item"> <a class="nav-link" href="{{ route('divisi.index') }}">Divisi</a>
+                        </li>
+                        <li class="nav-item"> <a class="nav-link" href="{{ route('departemen.index') }}">Departemen</a>
+                        </li>
+                        <li class="nav-item"> <a class="nav-link" href="{{ route('seksi.index') }}">Seksi</a>
+                        </li>
+                        <li class="nav-item"> <a class="nav-link" href="{{ route('struktur.index') }}">Relasi
+                                Struktur</a>
+                        </li>
+                        <li class="nav-item"> <a class="nav-link" href="{{ route('tipe-barang.index') }}">Tipe
+                                Barang</a>
+                        </li>
+                        <li class="nav-item"> <a class="nav-link" href="{{ route('tipe-pekerjaan.index') }}">Tipe
+                                Pekerjaan</a>
+                        </li>
+                        <li class="nav-item"> <a class="nav-link" href="{{ route('tipe-permit.index') }}">Tipe
+                                Permit</a>
+                        </li>
+                        <li class="nav-item"> <a class="nav-link" href="{{ route('tipe-equipment.index') }}">Tipe
+                                Equipment</a>
+                        </li>
+                        <li class="nav-item"> <a class="nav-link" href="{{ route('tipe-employee.index') }}">Tipe
+                                Employee</a>
+                        </li>
+                        <li class="nav-item"> <a class="nav-link" href="{{ route('satuan.index') }}">Satuan</a>
+                        </li>
+                        <li class="nav-item"> <a class="nav-link" href="{{ route('gender.index') }}">Gender</a>
+                        </li>
+                        <li class="nav-item"> <a class="nav-link" href="{{ route('jabatan.index') }}">Jabatan</a>
+                        </li>
+                        <li class="nav-item"> <a class="nav-link" href="{{ route('role.index') }}">Role</a>
+                        </li>
+                        <li class="nav-item"> <a class="nav-link" href="{{ route('arah.index') }}">Arah</a>
+                        </li>
+                        <li class="nav-item"> <a class="nav-link"
+                                href="{{ route('perusahaan.index') }}">Perusahaan</a>
+                        </li>
+                        <li class="nav-item"> <a class="nav-link" href="{{ route('barang.index') }}">Barang</a>
+                        </li>
+                        <li class="nav-item"> <a class="nav-link"
+                                href="{{ route('equipment.index') }}">Equipment</a>
+                        </li>
+                    </ul>
+                </div>
+            </li>
+        @endif
     </ul>
 </nav>
