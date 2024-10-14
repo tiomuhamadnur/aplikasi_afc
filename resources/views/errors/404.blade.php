e60e83e77d791a5160366ad63640fa61e9a0b70b
Dio Fadillah <dio.fadillah@gmail.com>
2024-10-14 10:46:30 +0700

    Error Page

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -6,7 +6,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <title>404</title>
+  <title>Page Not Found ! - 404</title>
 
   <style id="" media="all">
     /* cyrillic-ext */
--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -6,7 +6,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <title>404</title>
+  <title>Something Went Wrong - 500</title>
 
   <style id="" media="all">
     /* cyrillic-ext */
