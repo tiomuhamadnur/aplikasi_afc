0defbeb6d17ce1b338315b403afbe5c7ee78ff05
Tio Muhamad Nur <outsider.kuningan52@gmail.com>
2025-01-20 14:13:45 +0700

    add feature: websocket

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -28,24 +28,6 @@ return [
     */
 
     'connections' => [
-        // 'pusher' => [
-        //     'driver' => 'pusher',
-        //     'key' => env('PUSHER_APP_KEY'),
-        //     'secret' => env('PUSHER_APP_SECRET'),
-        //     'app_id' => env('PUSHER_APP_ID'),
-        //     'options' => [
-        //         'cluster' => env('PUSHER_APP_CLUSTER'),
-        //         'host' => env('PUSHER_HOST') ?: 'api-'.env('PUSHER_APP_CLUSTER', 'mt1').'.pusher.com',
-        //         'port' => env('PUSHER_PORT', 443),
-        //         'scheme' => env('PUSHER_SCHEME', 'https'),
-        //         'encrypted' => true,
-        //         'useTLS' => true,
-        //     ],
-        //     'client_options' => [
-        //         // Guzzle client options: https://docs.guzzlephp.org/en/stable/request-options.html
-        //     ],
-        // ],
-
         'pusher' => [
             'driver' => 'pusher',
             'key' => env('PUSHER_APP_KEY'),
@@ -53,10 +35,14 @@ return [
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
+                'host' => env('PUSHER_HOST') ?: 'api-'.env('PUSHER_APP_CLUSTER', 'mt1').'.pusher.com',
+                'port' => env('PUSHER_PORT', 443),
+                'scheme' => env('PUSHER_SCHEME', 'https'),
                 'encrypted' => true,
-                'host' => env('PUSHER_HOST'),
-                'port' => env('PUSHER_PORT'),
-                'scheme' => env('PUSHER_SCHEME'),
+                'useTLS' => true,
+            ],
+            'client_options' => [
+                // Guzzle client options: https://docs.guzzlephp.org/en/stable/request-options.html
             ],
         ],
 
