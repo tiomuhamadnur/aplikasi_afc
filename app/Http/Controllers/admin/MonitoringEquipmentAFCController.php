60ec547b14d991205b65a6950c4a6e054e20e84e
Tio Muhamad Nur <outsider.kuningan52@gmail.com>
2025-04-20 11:37:45 +0700

    feat: whatsapp notif monitoring equipment afc

--- app/Http/Controllers/admin/MonitoringEquipmentAFCController.php
+++ app/Http/Controllers/admin/MonitoringEquipmentAFCController.php
@@ -17,7 +17,7 @@ class MonitoringEquipmentAFCController extends Controller
     const EQUIPMENT_TYPE_PG = 'PG';
 
     protected $sshTimeout = 3;
-    protected $pingTimeout = 2;
+    protected $pingTimeout = 3;
     protected $concurrency = 25;
 
     public function index()
@@ -355,7 +355,7 @@ class MonitoringEquipmentAFCController extends Controller
     public function sendMonitoringNotification()
     {
         // Ambil data user yang akan menerima notifikasi
-        $recipients = User::whereIn('id', [1]) //sesuaikan jumlahnya
+        $recipients = User::whereIn('id', [1, 2, 5]) //sesuaikan jumlahnya
                     ->with(['gender', 'relasi_struktur.departemen', 'relasi_struktur.seksi'])
                     ->whereNotNull('no_hp')
                     ->get();
